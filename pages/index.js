3f39b3371557284d045ecf8898078bfb2429d51a
Maycon Douglas <hellomaycondouglas@gmail.com>
2025-10-06 18:30:03 -0300

    adiciona `.prettierignore`

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,4 @@ function Home() {
   return <h1>Lame you! Bastante ❤</h1>;
 }
 
-function teste() {
-  console.log("Teste!");
-}
-
-function teste2() {
-  console.log("teste2!");
-}
-
 export default Home;
