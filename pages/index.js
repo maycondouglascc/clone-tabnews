c1ade2307c4e9af516c2e78395e0808af497521f
Maycon Douglas <hellomaycondouglas@gmail.com>
2025-10-06 18:22:48 -0300

    adiciona scriptions `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-  return <h1>Lame you! Bastante ❤</h1>
+  return <h1>Lame you! Bastante ❤</h1>;
+}
+
+function teste() {
+  console.log("Teste!");
+}
+
+function teste2() {
+  console.log("teste2!");
 }
 
 export default Home;
